99d9a3359594528190b78c9763d621d1dc2dd5d5
Dasabae <85019441+iamherok@users.noreply.github.com>
2022-03-22 10:57:36 +0530

    Update chat_event.js

--- event/chat_event.js
+++ event/chat_event.js
@@ -4,6 +4,7 @@ const { checkData } = require("./database/group_setting")
 const djs = require("@discordjs/collection")
 const { color } = require("../utils")
 const Levels = require('discord-xp')
+const axios = require("axios")
 const { QuickDB } = require("quick.db");
 const db = QuickDB(); 
 Levels.setURL("mongodb+srv://das77:das@cluster0.itbgi.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
